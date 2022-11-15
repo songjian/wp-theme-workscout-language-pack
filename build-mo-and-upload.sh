f6a3839957c589f091821bb635545b5b52bcde2b
sj <songjian@codeorder.cn>
2022-11-15 14:28:40 +0800

    翻译

--- build-mo-and-upload.sh
+++ build-mo-and-upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+msgfmt zh_CN.po -o zh_CN.mo
 rsync -av zh_CN.mo hk2:/wwwphp/jobs/wp-content/themes/workscout/languages/
 ssh hk2 chown www:www /wwwphp/jobs/wp-content/themes/workscout/languages/zh_CN.mo
 
